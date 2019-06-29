3c62544b0c5aac1ef4a597228ddc0eed0b63cffb
Jimmy Pang <davnnis2003@gmail.com>
2019-06-29 17:27:48 +0200

    testing PR from personal laptop

--- test.py
+++ test.py
@@ -2,4 +2,9 @@
 print ('hey sup')
 print ('')
 print ('sup sup')
+<<<<<<< HEAD
 print ('yeahhhh')
+=======
+print ('')
+print ('testing personal laptop connection to Github')
+>>>>>>> testing PR from personal laptop
