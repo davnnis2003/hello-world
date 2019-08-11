09f06df22acd3851cbbb21d605bc02010a15b328
Jimmy Pang <jimmy.pang@deliveryhero.com>
2019-08-11 17:12:22 +0200

    test the import class from another file

---
+++ functions.py
@@ -0,0 +1,6 @@
+class function_container:
+    def say_handsome():
+        return ('handsome!')
+
+function_container.say_handsome()
+#print('handsome!')
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
+from functions import function_container as f
+
 #Python
-print ('hey sup')
-print ('')
-print ('sup sup')
-print ('yeahhhh')
-print ('')
-print ('testing personal laptop connection to Github')
+# print ('hey sup')
+# print ('')
+# print ('sup sup')
+# print ('yeahhhh')
+# print ('')
+# print ('testing personal laptop connection to Github')
+
+text = f.say_handsome()
+print (text)
