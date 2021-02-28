6abdea66cbbcf52b525de09bfdbd28ba58a436a7
Jimmy Pang <davnnis2003@gmail.com>
2021-02-28 13:09:19 +0100

    2nd try out

--- yaml_test_python_file.py
+++ yaml_test_python_file.py
@@ -1,9 +1,14 @@
+# import yaml
+#
+# path_to_yaml_file = ('/Users/davnnis2003/PycharmProjects/jimmy-playground/config.yaml')
+# print(path_to_yaml_file)
+#
+# with open(r'/Users/davnnis2003/PycharmProjects/jimmy-playground/config.yaml') as file:
+#     fruits_list = yaml.load(file, Loader=yaml.FullLoader)
+#
+#     print(fruits_list)
+
 import yaml
-
-path_to_yaml_file = ('/Users/davnnis2003/PycharmProjects/jimmy-playground/config.yaml')
-print(path_to_yaml_file)
-
-with open(r'/Users/davnnis2003/PycharmProjects/jimmy-playground/config.yaml') as file:
-    fruits_list = yaml.load(file, Loader=yaml.FullLoader)
-
-    print(fruits_list)
+with open('tree.yaml') as f:
+    # use safe_load instead load
+    dataMap = yaml.safe_load(f)
